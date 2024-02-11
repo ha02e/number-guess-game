b48e38a34a5380f797f0161e335496f8e02fef19
hayoung <ha02e@naver.com>
2024-02-11 23:36:23 +0900

    5번 기회 기능 추가

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 
 //Reset 버튼을 누르면 게임 리셋
 
-//기회는 5번, 기회를 다쓰면 게임 끝
+//기회는 5번, 기회를 다쓰면 게임 끝(더이상 추측 불가, 버튼 disable)
 
 //유저가 1~100 범위 밖 숫자를 입력하면 알려준다. 기회 깎지 않는다.
 
@@ -21,6 +21,9 @@ let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
 let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
@@ -33,6 +36,10 @@ function pickRandomNum() {
 function play() {
   let userValue = userInput.value; //유저가 입력한 숫자
 
+  chances--;
+  chanceArea.textContent = `남은 기회 : ${chances}번`;
+  console.log("chance", chances);
+
   if (userValue < computerNum) {
     resultArea.textContent = "Up!!";
     //console.log("Up!!");
@@ -43,6 +50,14 @@ function play() {
     resultArea.textContent = "정답입니다!!";
     //console.log("정답입니다!!");
   }
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
 }
 
 function reset() {
@@ -50,6 +65,8 @@ function reset() {
   userInput.value = "";
   //새로운 랜덤 번호가 생성
   pickRandomNum();
+
+  resultArea.textContent = "결과값이 여기 나옵니다";
 }
 
 pickRandomNum();
