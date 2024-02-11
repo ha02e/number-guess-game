10c31e646fe695add66fa4653690a19b0ff67773
hayoung <ha02e@naver.com>
2024-02-11 23:00:30 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//랜덤번호 지정
+
+//유저가 번호를 입력하고 go 버튼을 누름
+
+//유저가 랜덤번호를 맞추면, 맞췄습니다!
+
+//랜덤번호 < 유저 번호, Down!!
+
+//랜덤번호 > 유저 번호, Up!!
+
+//Reset 버튼을 누르면 게임 리셋
+
+//기회는 5번, 기회를 다쓰면 게임 끝
+
+//유저가 1~100 범위 밖 숫자를 입력하면 알려준다. 기회 깎지 않는다.
+
+//유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회 깎지 않는다.
+
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+
+playButton.addEventListener("click", play);
+
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", computerNum);
+}
+
+function play() {
+  console.log("게임시작");
+}
+
+pickRandomNum();
