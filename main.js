496e4e2d8b37e527ce8c2a71d16543682905e43e
hayoung <ha02e@naver.com>
2024-02-13 00:22:24 +0900

    CSS 추가

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-//랜덤번호 지정
-
-//유저가 번호를 입력하고 go 버튼을 누름
-
-//유저가 랜덤번호를 맞추면, 맞췄습니다!
-
-//랜덤번호 < 유저 번호, Down!!
-
-//랜덤번호 > 유저 번호, Up!!
-
-//Reset 버튼을 누르면 게임 리셋
-
-//기회는 5번, 기회를 다쓰면 게임 끝(더이상 추측 불가, 버튼 disable)
-
-//유저가 1~100 범위 밖 숫자를 입력하면 알려준다. 기회 깎지 않는다.
-
-//유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회 깎지 않는다.
-
 let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
@@ -28,43 +10,39 @@ let history = [];
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
-//input창에 focus하면 전에 입력한 값 초기화
 userInput.addEventListener("focus", () => {
   userInput.value = "";
 });
 
 function pickRandomNum() {
-  computerNum = Math.floor(Math.random() * 10) + 1;
-  console.log("정답", computerNum);
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  //console.log("정답", computerNum);
 }
 
 function play() {
-  let userValue = userInput.value; //유저가 입력한 숫자
+  let userValue = userInput.value;
 
   if (userValue < 1 || userValue > 100) {
-    resultArea.textContent = "1과 100 사이 숫자를 입력해주세요.";
+    resultArea.textContent = "1~100 사이 숫자를 입력해주세요.";
     return;
   }
 
   if (history.includes(userValue)) {
-    resultArea.textContent("이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.");
+    resultArea.textContent =
+      "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.";
     return;
   }
 
   chances--;
   chanceArea.textContent = `남은 기회 : ${chances}번`;
-  console.log("chance", chances);
 
   if (userValue < computerNum) {
     resultArea.textContent = "Up!!";
-    //console.log("Up!!");
   } else if (userValue > computerNum) {
     resultArea.textContent = "Down!!";
-    //console.log("Down!!");
   } else {
     resultArea.textContent = "정답입니다!!";
     gameOver = true;
-    //console.log("정답입니다!!");
   }
 
   history.push(userValue);
@@ -79,12 +57,15 @@ function play() {
 }
 
 function reset() {
-  //user input 창이 깨끗하게 정리됨
   userInput.value = "";
-  //새로운 랜덤 번호가 생성
+
   pickRandomNum();
 
-  resultArea.textContent = "결과값이 여기 나옵니다";
+  gameOver = false;
+  playButton.disabled = false;
+  chances = 10;
+
+  resultArea.textContent = "1~100 숫자를 입력해주세요.";
 }
 
 pickRandomNum();
